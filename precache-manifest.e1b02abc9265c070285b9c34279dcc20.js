b4ee6430f60a82d8dc886096a6769b2fbd157a56
ganesh.mane <2023bit501@sggs.ac.in>
2024-12-31 06:31:50 +0530

    Updates

---
+++ precache-manifest.e1b02abc9265c070285b9c34279dcc20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c626cccadbea8169b3a60623b7e7594a",
+    "url": "/React-TextUtils/index.html"
+  },
+  {
+    "revision": "ed18f7bbb680b00879b8",
+    "url": "/React-TextUtils/static/css/main.57e66f2f.chunk.css"
+  },
+  {
+    "revision": "cbe69779581b979e23e5",
+    "url": "/React-TextUtils/static/js/2.ac609677.chunk.js"
+  },
+  {
+    "revision": "4711efca2c42b934fbd541131bbe1d37",
+    "url": "/React-TextUtils/static/js/2.ac609677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "771a1274f51369ca2aca",
+    "url": "/React-TextUtils/static/js/3.791bb123.chunk.js"
+  },
+  {
+    "revision": "ed18f7bbb680b00879b8",
+    "url": "/React-TextUtils/static/js/main.5d4ef03e.chunk.js"
+  },
+  {
+    "revision": "3e42e2f4ae43b63c9108",
+    "url": "/React-TextUtils/static/js/runtime-main.a6ca93c9.js"
+  }
+]);
